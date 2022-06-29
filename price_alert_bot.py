a65f3232c75a0a88bdb1fcad32e231a1921ff7e0
Brando-Commando <brandon.t.599@gmail.com>
2022-06-28 22:00:12 -0600

    Assign Command Test

--- price_alert_bot.py
+++ price_alert_bot.py
@@ -26,12 +26,11 @@ Optional Phase B:
 
 import os
 from dotenv import load_dotenv
-import random
 from discord.ext import commands
-import requests
-import json
-import time
+from numpy import true_divide
+import asyncio
 import yfinance as yahooFinance
+import traceback
 
 
 
@@ -57,7 +56,7 @@ bot = commands.Bot(command_prefix='-')
 # PRICE CHECK COMMAND
 ############################
 
-# This command will be structured like "!check Bitcoin" and will return a price.
+# This command will be structured like "-check Bitcoin" and will return a price.
 @bot.command(name='check', help='This checks the current price of a security. Use the symbol (TSLA, AAPL) for a crypto, use symbol and end with -USD (BTC-USD, MANA-USD).')
 async def response(ctx, search_name):
 
@@ -71,8 +70,11 @@ async def response(ctx, search_name):
         current_data = ('{:,}'.format(current_data))
 
         # Return message
-        message = (search_name + " has been last reported at $" + str(current_data))
+        # message = (search_name + " has been last reported at $" + str(current_data))
+        message = ("```bash\n" + ' "' + search_name + " has been last reported at $ " + str(current_data) + '"' + ' ```')
+
         await ctx.send(message)
+
 
     # Error message
     except: 
@@ -80,60 +82,83 @@ async def response(ctx, search_name):
 
 
 
-'''        
+
 
 ############################
 # ASSIGN COMMAND
 ############################
 
 # This command is to assign a stock or crypto to be monitored
-# WIP
-
-
-@bot.command(name='assign')
-async def response(ctx, search_name, time_delay: int, time_delay_type: str):
+@bot.command(name='assign', help='This will assign a security/crypto to a time based alert. EX: -assign AAPL 1 minute, or -assign AAPL 1 hour')
+async def response(ctx, search_name, time_delay: int, time_delay_type):
 
     try:
         ticker = yahooFinance.Ticker(search_name)
 
-        while True:
-
             # Time_delay = integer, time_delay_type = minutes or hour
+            
+        #    if time_delay_type == "hour" or "hours":
+        #        delay_type = 3600
 
-            if time_delay_type == "hour" or "hours":
-                delay_type = 3600
+
+        # an hour equals 3600 seconds
+        if time_delay_type in ("hour", "hours"):
+            delay_type = 3600
+
+        #    elif time_delay_type == "minute" or "minutes":
+        #        delay_type = 60
+
+        # a minute equals 60 seconds
+        elif time_delay_type in ("minute", "minutes"):
+            delay_type = 60
                 
+            
+        else: 
+            await ctx.send("That was not a valid measure of time.")
 
-            elif time_delay_type == "minute" or "minutes":
-                delay_type = 60
-                
-
-            else: 
-                await ctx.send("That was not a valid measure of time.")
+        reminder_message = ("We will update you every " + str(time_delay) + " " + time_delay_type)
+        await ctx.send(reminder_message)
 
 
+        while True:
             current_data = ticker.history(period='id')
             # Rounds to two decimal places
             current_data = round(current_data['Close'][0], 3)
             # Adds comma to help format the number
             current_data = ('{:,}'.format(current_data))
 
-            message = (search_name + " has been last reported at $" + str(current_data))
+
+            # the ```bash\n is what formats the text into cyan and a codeblock, the extra " is needed to format text. 
+            # color formatted text is used in the following format in one message:
+                # ```bash 
+                #   "test message"
+                # ```
+            # this is recreating the format in text.
+            message = ("```bash\n" + ' "' + search_name + " has been last reported at $ " + str(current_data) + '"' + ' ```')
             await ctx.send(message)
 
-            reminder_message = ("We will update you every " + time_delay + " " + time_delay_type)
-            await ctx.send(reminder_message)
 
             # Time delay function
-            time.sleep(time_delay * delay_type)
+            # asyncio allows other processes to run as normal while time halts the entire program,
+            # I am not sure if asyncio is needed with discord but I havent tested this with just time.
+            # This statement uses the string for data type turned into the integer delay type, multiplied by the 
+            # stated time_delay
+            await asyncio.sleep(time_delay * delay_type)
+
         
+
+    # This is a catch all except, an error is ran everytime the -assign command is used. 
+    # traceback.print_exec() prints the error traceback into the terminal. This can also be used to export all error
+    # messages into a database or other external format. 
     except:    
         await ctx.send("That was not a valid security/crypto, for a security use their symbol, for crypto end with -USD.")
+        traceback.print_exc()
 
 
-'''
 
-
+############################
+# 
+############################
 
 
 # Bot token required for start.
